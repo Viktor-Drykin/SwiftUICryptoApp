2bf6eef61fb0a20fb2fdb5ad24d74e16c2e8dcc2
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-09-01 17:47:01 +0100

    add statistics view

---
+++ SwiftUICryptoApp/SwiftUICryptoApp/Core/Components/StatisticView.swift
@@ -0,0 +1,43 @@
+//
+//  StatisticView.swift
+//  SwiftUICryptoApp
+//
+//  Created by Viktor Drykin on 01.09.2024.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+
+    let stat: StatisticModel
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+            HStack(spacing: 4) {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                Text(stat.percentageChange?.asPercentString() ?? "")
+                    .font(.caption)
+                .bold()
+            }
+            .foregroundStyle((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    Group {
+        StatisticView(stat: DeveloperPreview.instance.positiveStat)
+        StatisticView(stat: DeveloperPreview.instance.noPercentage)
+        StatisticView(stat: DeveloperPreview.instance.negativeStat)
+            .preferredColorScheme(.dark)
+    }
+}
